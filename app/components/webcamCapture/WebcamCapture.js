8515c1134e22b983970b63a9ffdcace271a314d5
eric <luna8418@gmail.com>
2018-10-17 12:16:22 +1100

    Hello World

---
+++ app/components/webcamCapture/WebcamCapture.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Webcam from 'react-webcam';
+import './style.scss';
+
+export default class WebcamCapture extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      screenshot: null
+    };
+  }
+
+  setRef = (webcam) => {
+    this.webcam = webcam;
+  };
+
+  capture = () => {
+    const imageSrc = this.webcam.getScreenshot();
+    this.setState({ screenshot: imageSrc });
+    const url = 'https://us-central1-emotify-219203.cloudfunctions.net/http';
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        content: imageSrc.substr(23)
+      })
+    };
+    fetch(url, options).then((res) => console.log(res.json()));
+    // console.log(imageSrc.substr(23));
+  };
+
+  render() {
+    const videoConstraints = {
+      width: 1280,
+      height: 720,
+      facingMode: 'user'
+    };
+
+    return (
+      <div>
+        <div className="wrapper">
+          <Webcam
+            audio={false}
+            height={550}
+            ref={this.setRef}
+            screenshotFormat="image/jpeg"
+            width={550}
+            videoConstraints={videoConstraints}
+          />
+          {this.state.screenshot ? <img src={this.state.screenshot} className="snapshot" alt="snapshot" /> : null}
+        </div>
+        <button onClick={this.capture} >Capture photo</button>
+      </div>
+    );
+  }
+}
+
+WebcamCapture.propTypes = {
+  // onCapture: PropTypes.func.isRequired
+};
