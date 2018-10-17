0927a7ac9503c0783acbdea5c890ac398eca5f8c
nnonsung <nnonsung@dius.com.au>
2018-10-17 13:38:27 +1100

    Add sentiment text

--- app/components/webcamCapture/WebcamCapture.js
+++ app/components/webcamCapture/WebcamCapture.js
@@ -7,7 +7,8 @@ export default class WebcamCapture extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      screenshot: null
+      screenshot: null,
+      data: {}
     };
   }
 
@@ -28,9 +29,40 @@ export default class WebcamCapture extends React.Component {
         content: imageSrc.substr(23)
       })
     };
-    fetch(url, options).then((res) => console.log(res.json()));
-    // console.log(imageSrc.substr(23));
+    fetch(url, options).then((res) => {
+      res.json().then(data => {
+        this.setState({
+          data
+        });
+      });
+    });
   };
+
+  sentimentText() {
+    const {
+      data
+    } = this.state;
+
+    if (!data.faceSentiments) {
+      return;
+    }
+
+    const face = data.faceSentiments[0];
+
+    if (!face) {
+      return 'No faces detected';
+    }
+
+    switch (face.happyIndex) {
+      case 1:
+        return 'You are happy ;)';
+        break;
+      case -1:
+        return 'You are unhappy ;(';
+      default:
+        return 'You have no feeling :|'
+    }
+  }
 
   render() {
     const videoConstraints = {
@@ -53,6 +85,7 @@ export default class WebcamCapture extends React.Component {
           {this.state.screenshot ? <img src={this.state.screenshot} className="snapshot" alt="snapshot" /> : null}
         </div>
         <button onClick={this.capture} >Capture photo</button>
+        <p>{this.sentimentText()}</p>
       </div>
     );
   }
